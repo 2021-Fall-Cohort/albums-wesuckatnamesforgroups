e1f73089ca617c7b363beb5117f3ff2c8e85ed3b
NathanielNorris <nnorris23@gmail.com>
2021-11-10 12:03:52 -0500

    http get, added song1

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -3,6 +3,7 @@ package org.wcci.apimastery;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.wcci.apimastery.Models.Album;
+import org.wcci.apimastery.Models.Song;
 import org.wcci.apimastery.Repositories.AlbumRepository;
 import org.wcci.apimastery.Repositories.SongRepository;
 
@@ -21,7 +22,8 @@ public class Populator implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Album album1 = new Album("this album", "Url...", "music Label", "no comments", "5 stars");
         albumRepo.save(album1);
-
-
+        Song song1 = new Song("And We Go Gentle", "3.00", "Dope Song", "5", album1);
+        songRepo.save(song1);
     }
+
 }
